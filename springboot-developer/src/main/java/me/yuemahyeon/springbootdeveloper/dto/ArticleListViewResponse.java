25c24d619b4459b47e9a1672602453d169b31968
염아연 <dus1094@naver.com>
2025-05-15 15:41:54 +0900

    spring 1주차

---
+++ springboot-developer/src/main/java/me/yuemahyeon/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package me.yuemahyeon.springbootdeveloper.dto;
+
+import lombok.Getter;
+import me.yuemahyeon.springbootdeveloper.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
