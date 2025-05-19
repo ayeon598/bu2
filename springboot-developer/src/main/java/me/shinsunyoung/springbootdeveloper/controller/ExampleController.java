d8070fc59882c0d4c4b0cec397d924dd63439d61
SunshineMoonGit <sunshinemoongit@gmail.com>
2025-05-19 16:49:20 +0900

    Spring 1주차

---
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/ExampleController.java
@@ -0,0 +1,39 @@
+package me.shinsunyoung.springbootdeveloper.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        System.out.println("✅ 요청 들어옴"); // 로그 출력 추가
+
+        Person examplePerson = new Person();
+        examplePerson.setId(1L);
+        examplePerson.setName("홍길동");
+        examplePerson.setAge(11);
+        examplePerson.setHobbies(List.of("운동", "독서"));
+
+        model.addAttribute("person", examplePerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example-view"; // templates/example-view.html
+    }
+
+    @Setter
+    @Getter
+    static class Person {
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
